113a01ce1caf12cb95d9a88b38a70e4d0456d5ba
Scott Krstyen <scott.krstyen@micronet-inc.com>
2020-01-14 15:02:59 -0700

    Fix for multiple runs. On boot read in config file.

--- app/src/main/java/com/micronet/dsc/ats/BootReceiver.java
+++ app/src/main/java/com/micronet/dsc/ats/BootReceiver.java
@@ -13,6 +13,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.SystemClock;
+
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
@@ -27,7 +28,6 @@ public class BootReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-
         Log.d(TAG, "System Boot Notification");
 
         // Display the time in minutes and seconds since device boot
@@ -37,9 +37,29 @@ public class BootReceiver extends BroadcastReceiver {
                 TimeUnit.MILLISECONDS.toSeconds(elapsed) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(elapsed)));
         Log.d(TAG, "Elapsed time since boot: " + timeSinceBoot);
 
+        // Check for updated config files.
+        copyAlternateConfigFiles(context);
+
         // Send intent to service (and start if needed)
         Intent i = new Intent(context, MainService.class);
         i.putExtra(Power.BOOT_REQUEST_NAME, 1);
         context.startForegroundService(i);
+    }
+
+    public boolean copyAlternateConfigFiles(Context context) {
+        // Reset isNewConfig back to false in case activity is run again.
+        int config_files_updated = Config.init();
+        int eventcode_files_updated = CodeMap.init();
+
+        // now, if we did something, we should remember this in the state file so that we can
+        // generate a message the next time the ATS service starts (which could be right away if that is how we got here)
+        if ((config_files_updated | eventcode_files_updated) != 0) {
+            State state = new State(context);
+            Log.d(TAG, "New Configuration file detected.");
+            state.setFlags(State.PRECHANGED_CONFIG_FILES_BF, config_files_updated | eventcode_files_updated);
+            return true;
+        }else{
+            return false;
+        }
     }
 } // class
